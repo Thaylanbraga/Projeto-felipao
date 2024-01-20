885ff6534c2881f6ddce8ee214113c1f5f778c60
Thaylanbraga <thaylantiago@gmail.com>
2024-01-20 08:05:45 -0300

    codigo alterado de novo

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("olha só")
+let nome = "Thill"
+let experiencia = 15000
+
+if (experiencia <= 1000){
+    console.log("O héroi de nome" + nome + "esta no nível  de Ferro")
+} else if (experiencia > 1000 && experiencia <= 2000){
+    console.log("O héroi de nome" + nome + "esta no nível  de Bronze")
+}else if (experiencia > 2000 && experiencia <= 5000){
+    console.log("O héroi de nome" + nome + "esta no nível  de Prata")
+}else if (experiencia > 5000 && experiencia <= 7000){
+    console.log("O héroi de nome" + nome + "esta no nível  de Ouro")
+} else if (experiencia > 7000 && experiencia <= 8000){
+    console.log("O héroi de nome" + nome + "esta no nível  de Platina")
+}else if (experiencia > 8000 && experiencia <= 9000){
+    console.log("O héroi de nome" + nome + "esta no nível  de Ascendente")
+}else if (experiencia > 9000 && experiencia <= 10000){
+    console.log("O héroi de nome" + nome + "esta no nível  de Imortal")
+}else if (experiencia > 10000){
+    console.log("O héroi de nome" + nome + "esta no nível  de Radiante")
+}
